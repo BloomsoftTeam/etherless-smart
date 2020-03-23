1f17c536321cdd2a94a1e3772b68a4129794081f
Gabriele323 <garbingabriele1@gmail.com>
2020-03-23 17:04:39 +0100

    Integrazione di openzeppelin e fix

--- contracts/contractsInterface.sol
+++ contracts/contractsInterface.sol
@@ -1,38 +1,42 @@
 pragma solidity 0.5.16;
 
-contract ContractsInterface {
+import "@openzeppelin/contracts/ownership/Ownable.sol";
+
+contract ContractsInterface is Ownable {
+
+    //Ownable ha automaticamente un campo dati _owner che equivale a chi ha deployato il contratto (di default) e si può trasferire il "possesso" del contratto, fornisce il modifier onlyOwner
 
     mapping (string => address payable) public funOwnership;
     mapping (string => uint) public funPrices;
-    mapping (string => bool) public funHidden;
+    mapping (string => bool) public funAvailability;
 
-    address payable constant adminAddress = 0xA23E02B08003AD253fa5f217efCBa8D1213c687b;
+    function hasFunPermission(string memory fName, address payable devAddress) public returns(bool){
+        return funOwnership[fName] == devAddress;
+    }
 
-    function addFunOwnership(string memory fName, address payable developerAddress) public {
-        funOwnership[fName] = developerAddress;
+    function setFunOwnership(string memory fName, address payable devAddress) public returns(bool) {
+        if(funOwnership[fName] == 0){
+            funOwnership[fName] = devAddress;
+            return true;
+        }
+        return funOwnership[fName] == devAddress;
     }
     
-    function addFunPrice(string memory fName, uint fPrice) public {
+    function setFunPrice(string memory fName, uint fPrice) public onlyOwner {
         funPrices[fName] = fPrice;
     }
 
-    function addFunHidden(string memory fName, bool h) public {
-        funHidden[fName] = h;
+    function setFunAvailability(string memory fName, bool available) public onlyOwner {
+        funAvailability[fName] = available;
     }
 
-    function changeHidden(string memory fName) public {
-        funHidden[fName] = !funHidden[fName];
-    }
-
-    function removeFunOwnership(string memory fName) public {
-        delete funOwnership[fName];
-    }
-
-    function removeFunPrices(string memory fName) public {
-        delete funPrices[fName];
-    }
-
-    function removeFunHidden(string memory fName) public {
-        delete funHidden[fName];
+    function removeFunOwnership(string memory fName, address payable devAddress) public returns(bool) {
+        if(hasFunPermission(fName, devAddress)){
+            delete funOwnership[fName];
+            delete funPrices[fName];
+            delete funAvailability[fName];
+            return true;
+        }
+        return false;
     }
 }
--- contracts/deployContract.sol
+++ contracts/deployContract.sol
@@ -3,44 +3,49 @@ pragma solidity 0.5.16;
 import "contractsInterface.sol";
 //Ownable = openzeppelin
 contract DeployContract is ContractsInterface {
-    uint constant costoDeploy = 1759633996128805 wei; //sono 20 centesimi
+    uint constant deployFee = 1759633996128805 wei; //sono 20 centesimi
     
     mapping (string => address payable) tokenOwnership;
 
-    event uploadToken(string token, address payable userAddress, string fName, bool updateFun);
-    event requestUpload(address payable userAddress);
+    event uploadToken(string token, address payable devAddress, string fName, bool updateFun);
+    event requestUpload(address payable devAddress);
 
-    function addTokenOwnership(string memory token, address payable developerAddress) public {
-        tokenOwnership[token] = developerAddress;
+    function addTokenOwnership(string memory token, address payable devAddress) public {
+        tokenOwnership[token] = devAddress;
     }
 
-    function removeTokenOwnership(string memory token) public {
+    function removeTokenOwnership(string memory token) private {
         delete tokenOwnership[token];
     }
 
-    function deploy(string memory token, string memory fName, bool updateFun) payable public {
-        require(msg.sender.balance >= costoDeploy);
+    function deploy(string memory token, string memory fName) payable public {
+        require(msg.sender.balance >= deployFee);
+        bool updateFun = (funOwnership[fName] == msg.sender);
         addTokenOwnership(token, msg.sender);
         emit uploadToken(token, msg.sender, fName, updateFun); //ascoltato da EC2
     }
     
     //EC2 chiama
-    function sendRequestUpload(address payable userAddress) public {
-        emit requestUpload(userAddress);
+    function sendRequestUpload(address payable devAddress) public onlyOwner{
+        emit requestUpload(devAddress);
     }
     
     //EC2 chiama, calcolo del prezzo su server
-    function consumeToken(string memory token, string memory fName, address payable userAddress, uint fPrice) public payable {
-        removeTokenOwnership(token);
-        addFunOwnership(fName, userAddress);
-        addFunHidden(fName, false);
-        addFunPrice(fName, fPrice);
-        adminAddress.transfer(costoDeploy);
+    function consumeToken(string memory token, string memory fName, address payable devAddress, uint fPrice) public onlyOwner payable returns(bool) {
+        if(tokenOwnership[token] != 0x0){
+            removeTokenOwnership(token);
+            setFunOwnership(fName, usersAddress);
+            setFunAvailability(fName, true);
+            setFunPrice(fName, fPrice);
+            owner().transfer(deployFee);
+            return true;
+        }
+        return false;
     }
     
     function requestTokenRefund(string memory token) public payable {
         require(tokenOwnership[token] == msg.sender);
-        msg.sender.transfer(costoDeploy);
+        msg.sender.transfer(deployFee);
         removeTokenOwnership(token);
     }
 }
